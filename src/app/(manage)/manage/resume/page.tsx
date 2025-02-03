952dad8a0ef57540600baad4e5284f5579ea06a1
Pangho297 <monanazzang@gmail.com>
2025-02-03 21:45:44 +0900

    feat: sectionTitle 버튼 추가

--- src/app/(manage)/manage/resume/page.tsx
+++ src/app/(manage)/manage/resume/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
 import Container from "@/components/common/Container";
 import SectionTitle from "@/components/common/SectionTitle";
 
+interface FormType {
+  education: EducationDto[];
+  career: CareerDto[];
+  project: ProjectDto[];
+  portfolio: Portfolio[];
+}
+
+interface EducationDto {
+  schoolName: string;
+  admissionDate: Date;
+  graduationDate?: Date;
+}
+
+interface CareerDto {
+  companyName: string;
+  position: string;
+  description: string;
+  joinDate: Date;
+  quitDate?: Date;
+}
+
+interface ProjectDto {
+  projectName: string;
+  personnel: string;
+  skill: string[];
+  description: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+interface Portfolio {
+  type: "link" | "file";
+  file?: File;
+  url?: string;
+}
+
 export default function Resume() {
+  const {} = useForm<FormType>();
+
   return (
     <article className="flex w-full min-w-96 max-w-[1328px] flex-col gap-6 p-6">
       <Container>
@@ -32,9 +74,8 @@ export default function Resume() {
         </div>
         <div className="flex flex-col gap-3">
           <SectionTitle title="포트폴리오" />
-        </div>
-        <div>
-          
+          <p>유형</p>
+          <p>파일 또는 링크</p>
         </div>
       </Container>
     </article>
--- src/components/common/SectionTitle/index.tsx
+++ src/components/common/SectionTitle/index.tsx
@@ -1,13 +1,19 @@
+import { Button } from "../Button";
+import { Plus } from "lucide-react";
+
 interface SectionTitleProps {
   title: string;
 }
 
 export default function SectionTitle({ title }: SectionTitleProps) {
   return (
-    <div className="flex w-full flex-col border-b pb-3">
+    <div className="flex w-full flex-row items-center justify-between border-b pb-3">
       <h2 className="text-h2 font-bold" aria-label="section-heading">
         {title}
       </h2>
+      <Button className="size-8 rounded-sm p-0" variant="ghost">
+        <Plus className="size-4 text-gray-400" />
+      </Button>
     </div>
   );
 }
